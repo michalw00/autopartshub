3d17bf21315b2266359cf509ce357d294660ca15
Michał <wmichal133@gmail.com>
2025-02-14 20:51:04 +0100

    Initial commit

---
+++ src/test/java/com/autopartshub/AutopartshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopartshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopartshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
